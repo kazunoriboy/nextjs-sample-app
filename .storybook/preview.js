3baea1904e5071f7ff84496145947c8b5098e6bd
kazunoriboy <matunorigmnw@gmail.com>
2022-11-29 12:57:22 +0900

    storybookにThemeの適用、リセットCSS、next/imageの差し替えの設定を追加

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,8 @@
+import { addDecorator } from '@storybook/react'
+import { createGlobalStyle, ThemeProvidor } from 'styled-components'
+import { theme } from '../src/themes'
+import * as NextImage from 'next/image'
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -7,3 +12,44 @@ export const parameters = {
     },
   },
 }
+
+export const createGlobalStyle = createGlobalStyle`
+  html,
+  body,
+  textarea {
+    padding: 0;
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+  }
+  * {
+    box-sizing: border-box;
+  }
+  a {
+    text-decolation: none;
+    transition: .25s;
+    color: #000000;
+  }
+`
+
+addDecorator((story) => (
+  <ThemeProvidor theme={theme}>
+    <GlobalStyle />
+    {story()}
+  </ThemeProvidor>
+))
+
+const OriginalNextImage = NextImage.default;
+
+Object.defineProperty(NextImage, 'default', {
+  configurable: true,
+  value: (props) => typeof props.src === 'string' ? (
+    <OriginalNextImage {...props} unoptimized blurDataURL={props.src} />
+  ) : (
+    <OriginalNextImage {...props} unoptimaized />
+  )
+})
+
+Object.defineProperty(NextImage, '__esModule', {
+  configurable: true,
+  value: true,
+})
