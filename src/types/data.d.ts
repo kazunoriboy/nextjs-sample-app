9b3b94c81402daab513cbe258ea209b0ffa215f1
kazunoriboy <matunorigmnw@gmail.com>
2022-12-03 18:52:56 +0900

    baseUrlを設定することで、ファイルのインポートの基準をsrcにする

--- src/types/data.d.ts
+++ src/types/data.d.ts
@@ -1,29 +1,27 @@
-declare module 'types' {
-  export type Category = 'shoes' | 'clothes' | 'book'
-  export type Condition = 'new' | 'used'
+export type Category = 'shoes' | 'clothes' | 'book'
+export type Condition = 'new' | 'used'
 
-  export type User = {
-    id: number
-    username: string
-    displayName: string
-    email: string
-    profileImageUrl: string
-    description: string
-  }
+export type User = {
+  id: number
+  username: string
+  displayName: string
+  email: string
+  profileImageUrl: string
+  description: string
+}
 
-  export type Product = {
-    id: number
-    category: Category
-    title: string
-    description: string
-    imageUrl: string
-    blurDataUrl: string
-    price: number
-    condition: Condition
-    owner: User
-  }
+export type Product = {
+  id: number
+  category: Category
+  title: string
+  description: string
+  imageUrl: string
+  blurDataUrl: string
+  price: number
+  condition: Condition
+  owner: User
+}
 
-  export type ApiContext = {
-    apiRootUrl: string
-  }
+export type ApiContext = {
+  apiRootUrl: string
 }
---
+++ src/utils/styles.ts
@@ -0,0 +1,6 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/ban-types */
+import { theme } from 'themes'
+import type { ResponsiveProp, Responsive } from 'types'
+
+
