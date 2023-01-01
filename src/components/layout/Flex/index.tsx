d126f133a09f875270a9eabf5fc1016aa2ff099a
kazunoriboy <matunorigmnw@gmail.com>
2023-01-01 15:22:53 +0900

    簡単にflexを使用するためのラッパーコンポーネントを作成

---
+++ src/components/layout/Flex/index.tsx
@@ -0,0 +1,51 @@
+/* eslint-disable prettier/prettier */
+import styled from "styled-components";
+import Box, { BoxProps } from "components/layout/Box";
+import type {
+  Responsive,
+  CSSPropertyAlignItems,
+  CSSPropertyAlignContent,
+  CSSPropertyJustifyContent,
+  CSSPropertyJustifyItems,
+  CSSPropertyFlexDirection,
+  CSSPropertyJustifySelf,
+  CSSPropertyFlexWrap,
+  CSSPropertyAlignSelf
+} from 'types/styles'
+import { toPropValue } from "utils/styles";
+
+type FlexProps = BoxProps & {
+  alignItems?: Responsive<CSSPropertyAlignItems>
+  alingContent?: Responsive<CSSPropertyAlignContent>
+  justifyContent?: Responsive<CSSPropertyJustifyContent>
+  justifyItems?: Responsive<CSSPropertyJustifyItems>
+  flexWrap?: Responsive<CSSPropertyFlexWrap>
+  flexBasis?: Responsive<string>
+  flexDirection?: Responsive<CSSPropertyFlexDirection>
+  flexGrow?: Responsive<string>
+  flexShrink?: Responsive<string>
+  justifySelf?: Responsive<CSSPropertyJustifySelf>
+  alignSelf?: Responsive<CSSPropertyAlignSelf>
+  order?: Responsive<string>
+}
+
+const Flex = styled(Box)<FlexProps>`
+  ${(props) => toPropValue('align-items', props.alignItems, props.theme)}
+  ${(props) => toPropValue('aling-content', props.alingContent, props.theme)}
+  ${(props) => toPropValue('justify-content', props.justifyContent, props.theme)}
+  ${(props) => toPropValue('justify-items', props.justifyItems, props.theme)}
+  ${(props) => toPropValue('flex-wrap', props.flexWrap, props.theme)}
+  ${(props) => toPropValue('flex-basis', props.flexBasis, props.theme)}
+  ${(props) => toPropValue('flex-direction', props.flexDirection, props.theme)}
+  ${(props) => toPropValue('flex-grow', props.flexGrow, props.theme)}
+  ${(props) => toPropValue('flex-shirink', props.flexShrink, props.theme)}
+  ${(props) => toPropValue('justify-self', props.justifySelf, props.theme)}
+  ${(props) => toPropValue('align-self', props.alignSelf, props.theme)}
+  ${(props) => toPropValue('order', props.order, props.theme)}
+`
+
+Flex.defaultProps = {
+  display: 'flex'
+}
+
+export default Flex
