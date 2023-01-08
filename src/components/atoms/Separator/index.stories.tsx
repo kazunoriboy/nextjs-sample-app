626993ed1983788988d2d2678d36388e8faba8ef
kazunoriboy <matunorigmnw@gmail.com>
2023-01-08 22:53:03 +0900

    Separatorコンポーネントのストーリーを実装

---
+++ src/components/atoms/Separator/index.stories.tsx
@@ -0,0 +1,14 @@
+import { ComponentMeta } from '@storybook/react'
+import Separator from '.'
+
+export default {
+  title: 'Atoms/Separator',
+} as ComponentMeta<typeof Separator>
+
+export const Standard = () => (
+  <>
+    <Separator>or</Separator>
+    <Separator>and</Separator>
+    <Separator />
+  </>
+)
