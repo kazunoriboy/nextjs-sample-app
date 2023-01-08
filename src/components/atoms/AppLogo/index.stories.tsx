14a0678f9344d67750e79a53139e0aaab209293b
kazunoriboy <matunorigmnw@gmail.com>
2023-01-08 14:47:00 +0900

    formatを実施

--- src/components/atoms/AppLogo/index.stories.tsx
+++ src/components/atoms/AppLogo/index.stories.tsx
@@ -1,4 +1,4 @@
-import { ComponentMeta } from "@storybook/react";
+import { ComponentMeta } from '@storybook/react'
 import AppLogo from './index'
 
 export default { title: 'Atoms/AppLogo' } as ComponentMeta<typeof AppLogo>
