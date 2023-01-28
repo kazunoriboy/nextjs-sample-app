9dc6b81cb221b4182f55f7251d053472d6a84777
kazunoriboy <matunorigmnw@gmail.com>
2023-01-28 18:55:21 +0900

    既存の記述を修正

--- src/components/templates/Layout/index.tsx
+++ src/components/templates/Layout/index.tsx
@@ -1,13 +1,13 @@
-import Header from "components/organisms/Header"
-import Footer from "components/organisms/Footer"
-import Box from "components/layout/Box"
-import Separator from "components/atoms/Separator"
+import Separator from 'components/atoms/Separator'
+import Box from 'components/layout/Box'
+import Footer from 'components/organisms/Footer'
+import Header from 'components/organisms/Header'
 
 interface LayoutProps {
   children: React.ReactNode
 }
 
-const Layout = ({children}: LayoutProps) => {
+const Layout = ({ children }: LayoutProps) => {
   return (
     <>
       <Header />
