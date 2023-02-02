c9340aa4303daf0c61577be1554fcd06cb460b85
kazunoriboy <matunorigmnw@gmail.com>
2023-02-02 09:36:00 +0900

    設定ファイルのパスをミスっていたので修正

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ const createJestConfig = nextJest({ dir: './' })
 const customJestConfig = {
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  moduleDirectories: ['node_modeules', '<rootDir>/src'],
-  testEnvironment: 'jsdom'
+  moduleDirectories: ['node_modules', '<rootDir>/src'],
+  testEnvironment: 'jsdom',
 }
 module.exports = createJestConfig(customJestConfig)
